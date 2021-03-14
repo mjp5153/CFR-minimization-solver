edcbf1cf95e78b19bcb764801a18e0539566aa5d
Mike Peters <michael.peters@jhuapl.edu>
2021-03-14 15:16:09 -0400

    basic routing

---
+++ cfr-front-ng/src/app/main/main.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  constructor() { }
+
+  public year;
+
+  ngOnInit() {
+    this.year = new Date().getFullYear();
+  }
+
+}
